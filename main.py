1f5eab574af01a1f9dd2605d2a7e29da40e8f2cf
Forecastr <ng.telitsyn@proton.me>
2023-09-04 00:10:33 +0200

    ✨ Created an example of POST method

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # def home():
 #     return "Home"
 
+# An example of a GET route
 @app.route("/usr/<user_id>")
 def get_user(user_id):
     user_data = {"id": user_id, "name": "Mordecai", "species": "Bluejay", "hobby": "chilling"}
@@ -18,6 +19,14 @@ def get_user(user_id):
 
     response = {"status_code": 200, "result": user_data}
     # return jsonified user data together with 200 - a status code for successful response
+    return jsonify(response)
+
+# an example of a POST route
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    # Here we would ideally have writing the data to the database, but this is only a demo
+    response = {"status_code": 201, "result": data}
     return jsonify(response)
 
 if __name__ == "__main__":
