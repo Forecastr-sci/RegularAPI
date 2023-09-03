d96363c29c68f313683c0f0b3fcf2fe7d599b7d8
Forecastr <ng.telitsyn@proton.me>
2023-09-04 00:00:12 +0200

    ✨ added get_user route with extra optional param

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Home"
+# @app.route("/")
+# def home():
+#     return "Home"
+
+@app.route("/usr/<user_id>")
+def get_user(user_id):
+    user_data = {"id": user_id, "name": "Mordecai", "species": "Bluejay", "hobby": "chilling"}
+
+    # Checks if request had argument "extra", like this: "/usr/<user_id>?extra=whatever"
+    extra = request.args.get("extra")
+    # If argument was actually passed its value will be added to user data at key "extra"
+    if extra:
+        user_data["extra"] = extra
+
+    # return jsonified user data together with 200 - a status code for successful response
+    return jsonify(user_data), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
